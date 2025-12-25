997754c2c6f22e97beaab3bdfb507c0dbe022b50
Vladimir Varbanov <vlado@vlado1.com>
2025-12-25 18:39:18 +0200

    Refactor skills section and add mock data for experience and education

---
+++ skills.js
@@ -0,0 +1,30 @@
+import { skillItemsList } from "./mock-data/data.js";
+const skillBarWrapper = document.getElementById("skill-bar-wrapper");
+
+customElements.define("skill-item", class extends HTMLElement {
+    constructor() {
+        super();
+        const skillsTemplate = document.getElementById("skill-item-template");
+        console.log("skillsTemplate:", skillsTemplate);
+        const shadow = this.attachShadow({ mode: "open" });
+        const templateContent = skillsTemplate.content;
+        shadow.appendChild(document.importNode(templateContent, true));
+    }
+});
+
+skillItemsList.forEach((skill) => {
+    const skillItemElement = document.createElement("skill-item");
+    
+    const titleSlot = document.createElement("span");
+    titleSlot.setAttribute("slot", "title");
+    titleSlot.textContent = skill.title;
+
+    const percentSlot = document.createElement("span");
+    percentSlot.setAttribute("slot", "percent");
+    percentSlot.textContent = skill.level;
+
+    skillItemElement.appendChild(percentSlot);
+    
+    skillItemElement.appendChild(titleSlot);
+    skillBarWrapper.appendChild(skillItemElement);
+});
